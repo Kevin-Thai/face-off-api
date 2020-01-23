f80a3f8882fa3038a9364bfc8ad3e510b174ea95
Kevin Thai <kevin.k.thai@gmail.com>
2020-01-23 09:59:09 -0600

    Removed comments

--- server.js
+++ server.js
@@ -21,41 +21,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-{/*Old database before SQL
-const database = {
-  users: [
-    {
-      id:'123',
-      name: 'John',
-      password: 'cookies',
-      email: 'john@email.com',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id:'124',
-      name: 'Sally',
-      password: 'bananas',
-      email: 'sally@email.com',
-      entries: 0,
-      joined: new Date()
-    }
-  ],
-  // login: [
-  //   {
-  //     id: '987',
-  //     hash: '',
-  //     email:'john@gmail.com'
-  //   }
-  // ]
-}
-
-Code to test whether our connection to the database was working
-knex.select('*').from('users').then(data => {
-  console.log(data);
-});
-*/}
-
 app.get('/', (req, res) => { res.send(users) })
 app.post('/signin', signin.handleSignin(knex, bcrypt))
 app.post('/register', register.handleRegister(knex, bcrypt))
@@ -64,13 +29,5 @@ app.put('/image', (req, res) => {image.handleImage(req, res, knex)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
 app.listen(3000, ()=> {
-  console.log('works')
+  console.log('Server running on port 3000')
 })
-
-{/*
-/                 --> res = this is working
-/signin           --> POST (posting data, JSON; user information)  = success/fail
-/register         --> POST (add data to the database or variable with new user info) = user
-/profile/:userID  --> GET (get user information) = user
-/image            --> PUT (updates user profile) = user or change to user
-*/}
